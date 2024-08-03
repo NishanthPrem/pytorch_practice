3ec77d04dc5d9ed59496aaabbc83f7370202807f
Nishanth Prem <nishanthprem8@gmail.com>
2024-08-02 18:45:05 -0700

    Updated Code.

--- pytorch_classification.py
+++ pytorch_classification.py
@@ -1,4 +1,105 @@
 #%% Importing the libraries
 
+import pandas as pd
+import matplotlib.pyplot as plt
 import torch
+
+#%% Setting up the device
+
+RANDOM_SEED=42
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#%% Creating the dataset
+
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+
+n_samples = 1000
+X, y = make_moons(n_samples, random_state=RANDOM_SEED)
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+#%% Visualizing the data
+
+import matplotlib.pyplot as plt
+plt.scatter(x=X[:, 0], 
+            y=X[:, 1], 
+            c=y, 
+            cmap=plt.cm.RdYlBu);
+
+
+#%% Building the Model
 import torch.nn as nn
+
+class MoonModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(2, 5)
+        self.fc2 = nn.Linear(5, 5)
+        self.fc3 = nn.Linear(5, 1)
+        
+        self.activation = nn.ReLU()
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        x = self.activation(x)
+        
+        return self.fc3(x)
+
+model = MoonModel().to(device)
+
+#%% Setting up the loss function and optimizer
+
+import torch.optim as optim
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+#%% Training the data
+
+torch.manual_seed(RANDOM_SEED)
+
+epochs = 1000
+
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+for epoch in range(epochs):
+    y_logits = model(X_train).squeeze()
+    
+    y_pred = torch.round(torch.sigmoid(y_logits))
+    loss = loss_fn(y_logits, y_train)
+    
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    model.eval()
+    with torch.inference_mode():
+        test_logits = model(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        
+        test_loss = loss_fn(test_logits, test_pred)
+        
+        if epoch % 100 == 0:
+         print(f"Epoch: {epoch} | Train loss: {loss:.3f} | Test loss: {test_loss:.3f}")
+
+#%% Predicting the data
+
+model.eval()
+with torch.inference_mode():
+    y_preds = torch.round(torch.sigmoid(model(X_test))).squeeze()
+
+#%% Plotting the output
+from helper_functions import plot_predictions, plot_decision_boundary
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model, X_test, y_test) # model_3 = has non-linearity
